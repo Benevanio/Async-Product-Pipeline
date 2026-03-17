adde68684a78911a9ac6c98fb91462e2dfd30f46
Benevanio Santos <benevaniosantos930@gmail.com>
2026-03-17 14:10:04 -0300

    feat: add validator for product name and description to ensure they are not empty(Se eu achar mais um bug aqui vou desistir desse projeto, PQP)

--- src/model/products.model.js
+++ src/model/products.model.js
@@ -1,14 +1,23 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const productSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
         unique: true,
+        validate: {
+            validator: (value) => !validator.isEmpty(value),
+            message: 'name cannot be empty',
+        },
     },
     description: {
         type: String,
         required: true,
+        validate: {
+            validator: (value) => !validator.isEmpty(value),
+            message: 'description cannot be empty',
+        },
     },
     price: {
         type: Number,
